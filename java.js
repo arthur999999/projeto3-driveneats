6dd458ddfeb9e948ebfc39903655575a3caae657
arthur999999 <arthurhono.contato@gmail.com>
2022-08-15 11:28:12 -0300

    Versão Bônus

--- java.js
+++ java.js
@@ -37,14 +37,34 @@ function greened(a, b, c) {
     precsob = Number(c)
   }
 }
-
+function menuuu() {
+  const menuend = document.querySelector('.menuresum')
+  menuend.classList.add('javafun')
+  document.querySelector('.princ p').innerHTML = textprin
+  document.querySelector('.princ span').innerHTML = String(
+    precprin.toFixed(2)
+  ).replace('.', ',')
+  document.querySelector('.beb p').innerHTML = textbe
+  document.querySelector('.beb span').innerHTML = String(
+    precbe.toFixed(2)
+  ).replace('.', ',')
+  document.querySelector('.sob p').innerHTML = textsob
+  document.querySelector('.sob span').innerHTML = String(
+    precsob.toFixed(2)
+  ).replace('.', ',')
+  document.querySelector('.price span').innerHTML =
+    'R$ ' + String((precprin + precbe + precsob).toFixed(2)).replace('.', ',')
+}
 function zapzap() {
   if (xis == 3) {
+    let namer = prompt('Qual é seu nome?')
+    let homer = prompt('Qual é seu endereço?')
     let textow = `Olá, gostaria de fazer o pedido:\n- Prato: ${textprin}\n- Bebida: ${textbe} \n- Sobremesa: ${textsob}\nTotal: R$ ${(
       precprin +
       precbe +
       precsob
-    ).toFixed(2)}`
+    ).toFixed(2)}\n\nNome: ${namer}\nEndereço: ${homer}`
+    console.log(textow)
     let celular = '554891564265'
 
     textow = encodeURIComponent(textow)
@@ -54,4 +74,9 @@ function zapzap() {
       '_blank'
     )
   }
+}
+
+function cancelmenu() {
+  const menuend = document.querySelector('.menuresum')
+  menuend.classList.remove('javafun')
 }
